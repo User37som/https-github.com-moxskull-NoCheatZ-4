d0c2ccab871ca9c8cf52fd1ff494a521dda2c7b7
L-EARN <le.padellec.sylvain@gmail.com>
2016-07-01 20:16:32 +0200

    Update travis script

--- scripts/make-protobuf.sh
+++ scripts/make-protobuf.sh
@@ -20,7 +20,7 @@ else
 	echo "Configuring protobuf 2.5.0"
 	chmod +x autogen.sh
 	./autogen.sh
-	./configure --build=i686-pc-linux-gnu CFLAGS="-m32 -DNDEBUG" CXXFLAGS="-m32 -DNDEBUG" LDFLAGS=-m32
+	./configure --build=i686-pc-linux-gnu --prefix=/usr CFLAGS="-m32 -DNDEBUG" CXXFLAGS="-m32 -DNDEBUG" LDFLAGS=-m32
 	echo "Building protobuf 2.5.0"
 	make
 fi
