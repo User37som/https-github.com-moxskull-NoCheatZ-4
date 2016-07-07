User37som/https-github.com-moxskull-NoCheatZ-4

commit c6fd28c79025a45017d402f22bf74d9ed1a65d54

Fix Logger
- Fix bugs when writing into logfiles
- Add client connect/disconnect messages to logfiles
- Add informations to plugin DebugMessages

server-plugin/Code/plugin.cpp
@@ -354,12 +354,15 @@ void CNoCheatZPlugin::LevelInit ( char const *pMapName )
 //---------------------------------------------------------------------------------
 void CNoCheatZPlugin::ServerActivate ( SourceSdk::edict_t *pEdictList, int edictCount, int clientMax )
 {
-	DebugMessage ( "CNoCheatZPlugin::ServerActivate" );
+	DebugMessage ( Helpers::format("CNoCheatZPlugin::ServerActivate (pEdictList:%p, edictCount:%d, clientMax:%d)", edictCount, clientMax) );
 
 	Helpers::m_EdictList = pEdictList;
 	//Helpers::m_EdictList_csgo = pEdictList;
 	//Helpers::m_edictCount = edictCount;
 	//Helpers::m_clientMax = clientMax;
+
+	SourceSdk::InterfacesProxy::GetGameEventManager ()->AddListener ( Logger::GetInstance (), "player_connect", true );
+	SourceSdk::InterfacesProxy::GetGameEventManager ()->AddListener ( Logger::GetInstance (), "player_disconnect", true );
 
 	NczPlayerManager::GetInstance ()->LoadPlayerManager ();
 
@@ -408,11 +411,12 @@ void CNoCheatZPlugin::LevelShutdown ( void ) // !!!!this can get called multiple
 //---------------------------------------------------------------------------------
 void CNoCheatZPlugin::ClientActive ( SourceSdk::edict_t *pEntity )
 {
-	DebugMessage ( Helpers::format ( "CNoCheatZPlugin::ClientActive (%X -> %s)", pEntity, pEntity->GetClassName () ) );
-
 	NczPlayerManager::GetInstance ()->ClientActive ( pEntity );
 
 	PlayerHandler::const_iterator ph ( NczPlayerManager::GetInstance ()->GetPlayerHandlerByEdict ( pEntity ) );
+
+	DebugMessage ( Helpers::format ( "CNoCheatZPlugin::ClientActive (pEntity:%p -> pEntity->classname:%s -> clientname:%s)", pEntity, pEntity->GetClassName (), ph->GetName() ) );
+
 	if( ph >= SlotStatus::PLAYER_CONNECTED ) HookBasePlayer ( ph );
 	if( ph >= SlotStatus::BOT )
 	{
@@ -429,7 +433,7 @@ void CNoCheatZPlugin::ClientActive ( SourceSdk::edict_t *pEntity )
 //---------------------------------------------------------------------------------
 void CNoCheatZPlugin::ClientDisconnect ( SourceSdk::edict_t *pEntity )
 {
-	DebugMessage ( "CNoCheatZPlugin::ClientDisconnect" );
+	DebugMessage ( Helpers::format("CNoCheatZPlugin::ClientDisconnect (pEntity:%p -> pEntity->classname:%s -> clientname:%s)", pEntity, pEntity->GetClassName (), PlayerHandler::const_iterator ( NczPlayerManager::GetInstance ()->GetPlayerHandlerByEdict ( pEntity ) )->GetName() ));
 
 	WallhackBlocker::GetInstance ()->ClientDisconnect ( Helpers::IndexOfEdict ( pEntity ) );
 	NczPlayerManager::GetInstance ()->ClientDisconnect ( pEntity );
@@ -446,7 +450,7 @@ void CNoCheatZPlugin::ClientPutInServer ( SourceSdk::edict_t *pEntity, char cons
 		ValidationTester::GetInstance ()->ResetPlayerDataStruct ( pEntity );
 	}
 
-	DebugMessage ( Helpers::format ( "CNoCheatZPlugin::ClientPutInServer (%s -> %s) (Was already connected: %s)", pEntity->GetClassName (), playername, Helpers::boolToString ( stat != SlotStatus::INVALID ) ) );
+	DebugMessage ( Helpers::format ( "CNoCheatZPlugin::ClientPutInServer (pEntity:%p -> pEntity->classname:%s, playername:%s) (Was already connected: %s)", pEntity, pEntity->GetClassName (), playername, Helpers::boolToString ( stat != SlotStatus::INVALID ) ) );
 }
 
 //---------------------------------------------------------------------------------
@@ -476,7 +480,7 @@ SourceSdk::PLUGIN_RESULT CNoCheatZPlugin::ClientConnect ( bool *bAllowConnect, S
 	SpamConnectTester::GetInstance ()->ClientConnect ( bAllowConnect, pEntity, pszName, pszAddress, reject, maxrejectlen );
 	SpamChangeNameTester::GetInstance ()->ClientConnect ( bAllowConnect, pEntity, pszName, pszAddress, reject, maxrejectlen );
 
-	DebugMessage ( Helpers::format ( "CNoCheatZPlugin::ClientConnect (AllowConnect: %s, %X -> %s, %s, %s, %s", Helpers::boolToString ( *bAllowConnect ), pEntity, pEntity->GetClassName (), pszName, pszAddress, reject ) );
+	DebugMessage ( Helpers::format ( "CNoCheatZPlugin::ClientConnect (bAllowConnect:%s, pEntity:%p -> pEntity->classname:%s, pszName:%s, pszAddress:%s, reject:%s, maxrejectlen:%d", Helpers::boolToString ( *bAllowConnect ), pEntity, pEntity->GetClassName (), pszName, pszAddress, reject, maxrejectlen ) );
 
 	if( !*bAllowConnect )
 	{
@@ -505,14 +509,13 @@ SourceSdk::PLUGIN_RESULT CNoCheatZPlugin::ClientConnect ( bool *bAllowConnect, S
 //---------------------------------------------------------------------------------
 SourceSdk::PLUGIN_RESULT CNoCheatZPlugin::RT_ClientCommand ( SourceSdk::edict_t *pEntity, const SourceSdk::CCommand &args )
 {
-	DebugMessage ( Helpers::format ( "CNoCheatZPlugin::ClientCommand(%X -> %s, %s)", pEntity, pEntity->GetClassName (), args.GetCommandString () ) );
-
 	if( !pEntity || pEntity->IsFree () )
 	{
 		return SourceSdk::PLUGIN_CONTINUE;
 	}
 
 	PlayerHandler::const_iterator ph ( NczPlayerManager::GetInstance ()->GetPlayerHandlerByEdict ( pEntity ) );
+	DebugMessage ( Helpers::format ( "CNoCheatZPlugin::ClientCommand (pEntity:%p -> pEntity->classname:%s -> clientname:%s, args:%s)", pEntity, pEntity->GetClassName (), ph->GetName(), args.GetCommandString () ) );
 	if( ph >= SlotStatus::PLAYER_CONNECTED )
 	{
 		if( ConCommandTester::GetInstance ()->RT_TestPlayerCommand ( ph, args.GetCommandString () ) )
@@ -531,6 +534,7 @@ SourceSdk::PLUGIN_RESULT CNoCheatZPlugin::RT_ClientCommand ( SourceSdk::edict_t 
 //---------------------------------------------------------------------------------
 SourceSdk::PLUGIN_RESULT CNoCheatZPlugin::NetworkIDValidated ( const char *pszUserName, const char *pszNetworkID )
 {
+	DebugMessage ( Helpers::format ( "CNoCheatZPlugin::NetworkIDValidated (pszUserName:%s, pszNetworkID:%s)", pszUserName, pszNetworkID ) );
 	if( !SteamGameServer_BSecure () ) return SourceSdk::PLUGIN_CONTINUE;
 
 	ValidationTester::GetInstance ()->AddPendingValidation ( pszUserName, pszNetworkID );

server-plugin/Code/Systems/Logger.cpp
@@ -18,8 +18,6 @@
 #include <fstream>
 #include <iostream>
 
-#include "Interfaces/InterfacesProxy.h"
-
 #include "Misc/include_windows_headers.h"
 #include "Misc/Helpers.h"
 #include "Misc/temp_Metrics.h"
@@ -28,6 +26,12 @@
 
 void Logger::Push ( const char * msg )
 {
+	/*
+		We need to make a copy of msg because most of functions calling Push are using Helpers::format while Push is also using Helpers::format.
+		But because Helpers::format is using a static buffer, msg also points to this static buffer. Hence, calling Helpers::format here will just overwrite the message ...
+	*/
+	basic_string copy_msg ( msg );
+
 	int server_tick;
 	if( SourceSdk::InterfacesProxy::m_game == SourceSdk::CounterStrikeGlobalOffensive )
 	{
@@ -40,12 +44,21 @@ void Logger::Push ( const char * msg )
 
 	if( AutoTVRecord::GetInstance ()->IsRecording () )
 	{
-		const char* record_tick ( Helpers::format ( "%s.dem:Tick #%d", AutoTVRecord::GetInstance ()->GetRecordFilename ().c_str (), AutoTVRecord::GetInstance ()->GetRecordTick () ) );
-		m_msg.AddToTail ( Helpers::format ( "%s [Server Tick #%d, SourceTV : %s] %s", Helpers::getStrDateTime ( "%x %X" ), server_tick, record_tick, msg ) );
+		basic_string move_msg;
+		move_msg.reserve ( 255 );
+		move_msg.append ( Helpers::getStrDateTime ( "%x %X " ) );
+		move_msg.append ( Helpers::format ( "[ Server Tick #%d, SourceTV:%s.dem : Tick #%d ] ", server_tick, AutoTVRecord::GetInstance ()->GetRecordFilename ().c_str (), AutoTVRecord::GetInstance ()->GetRecordTick () ) );
+		move_msg.append ( copy_msg );
+		m_msg.AddToTail ( std::move( move_msg ) );
 	}
 	else
 	{
-		m_msg.AddToTail ( Helpers::format ( "%s [Server Tick #%d] %s", Helpers::getStrDateTime ( "%x %X" ), server_tick, msg ) );
+		basic_string move_msg;
+		move_msg.reserve ( 255 );
+		move_msg.append ( Helpers::getStrDateTime ( "%x %X " ) );
+		move_msg.append ( Helpers::format ( "[ Server Tick #%d] ", server_tick ) );
+		move_msg.append ( copy_msg );
+		m_msg.AddToTail ( std::move ( move_msg ) );
 	}
 
 	ProcessFilter::HumanAtLeastConnected filter_class;
@@ -61,7 +74,7 @@ void Logger::Push ( const char * msg )
 template <>
 void Logger::Msg<MSG_CONSOLE> ( const char * msg, int verbose /*= 0*/ )
 {
-	std::cout << prolog.c_str () << msg << "\n";
+	std::cout << prolog.c_str () << ' ' << Plat_FloatTime() << ' ' << msg << '\n';
 #ifdef WIN32
 	OutputDebugStringA ( prolog.c_str () );
 	OutputDebugStringA ( msg );
@@ -95,31 +108,37 @@ void Logger::Msg<MSG_LOG_CHAT> ( const char * msg, int verbose /*= 0*/ )
 template <>
 void Logger::Msg<MSG_WARNING> ( const char * msg, int verbose /*= 0*/ )
 {
-	basic_string m ( prolog );
-	m.append ( "WARNING : " ).append ( msg ).append ( '\n' );
-	std::cout << m.c_str ();
+	basic_string m1 ( "WARNING : " );
+	m1.append ( msg );
+	Push ( m1.c_str () );
+
+	basic_string m2 ( prolog );
+	m2.append ( m1 ).append ( '\n' );
+	std::cout << m2.c_str ();
 #ifdef WIN32
-	OutputDebugStringA ( m.c_str () );
+	OutputDebugStringA ( m2.c_str () );
 #endif
-	Push ( m.c_str () );
 }
 
 template <>
 void Logger::Msg<MSG_ERROR> ( const char * msg, int verbose /*= 0*/ )
 {
-	basic_string m ( prolog );
-	m.append ( "ERROR : " ).append ( msg ).append ( '\n' );
-	std::cout << m.c_str ();
+	basic_string m1 ( "ERROR : " );
+	m1.append ( msg );
+	Push ( m1.c_str () );
+
+	basic_string m2 ( prolog );
+	m2.append ( m1 ).append ( '\n' );
+	std::cout << m2.c_str ();
 #ifdef WIN32
-	OutputDebugStringA ( m.c_str () );
+	OutputDebugStringA ( m2.c_str () );
 #endif
-	Push ( m.c_str () );
 }
 
 template <>
 void Logger::Msg<MSG_HINT> ( const char * msg, int verbose /*= 0*/ )
 {
-	std::cerr << prolog.c_str () << Plat_FloatTime () << " : " << msg << "\n";
+	std::cerr << prolog.c_str () << Plat_FloatTime () << " : " << msg << '\n';
 }
 
 template <>
@@ -146,6 +165,23 @@ void Logger::Msg<MSG_DEBUG> ( const char * msg, int verbose /*= 0*/ )
 	if( verbose > 2 )
 	{
 		Msg<MSG_CONSOLE> ( basic_string ( "DEBUG : " ).append ( msg ) );
+	}
+}
+
+void Logger::FireGameEvent ( SourceSdk::IGameEvent * ev )
+{
+	// player_connect
+	// player_disconnect
+
+	char const * const event_name = ev->GetName () + 7;
+
+	if( *event_name == 'c' )
+	{
+		Msg<MSG_LOG> ( Helpers::format ( "Client connect : %s [%s - %s]", ev->GetString ( "name" ), ev->GetString ( "networkid" ), ev->GetString ( "address" ) ) );
+	}
+	else
+	{
+		Msg<MSG_LOG> ( Helpers::format ( "Client disconnect : %s [%s] %s", ev->GetString ( "name" ), ev->GetString ( "networkid" ), ev->GetString ( "reason" ) ) );
 	}
 }
 
@@ -191,5 +227,5 @@ void Logger::Flush ()
 
 void Helpers::writeToLogfile ( const basic_string &text )
 {
-	Logger::GetInstance ()->Push ( Helpers::format ( "At %f (Server Tick #%d) : %s", Plat_FloatTime (), Helpers::GetTickCount (), text.c_str () ) );
+	Logger::GetInstance ()->Push ( text.c_str () );
 }

server-plugin/Code/Systems/Logger.h
@@ -16,8 +16,9 @@
 #ifndef LOGGER_H
 #define LOGGER_H
 
-#include "Preprocessors.h"
+#include "Interfaces/InterfacesProxy.h"
 
+#include "Preprocessors.h"
 #include "Misc/temp_basicstring.h"
 #include "Misc/temp_singleton.h"
 #include "Containers/utlvector.h"
@@ -52,6 +53,7 @@ enum msg_type
 };
 
 class Logger :
+	public SourceSdk::IGameEventListener002,
 	public Singleton<Logger>
 {
 	typedef Singleton<Logger> singleton_class;
@@ -63,8 +65,12 @@ private:
 public:
 	Logger () : singleton_class (), m_msg (), prolog ( "[NoCheatZ " NCZ_VERSION_STR "] " )
 	{};
-	virtual ~Logger () override final
-	{};
+	virtual ~Logger () final
+	{
+		SourceSdk::InterfacesProxy::GetGameEventManager ()->RemoveListener ( this );
+	};
+
+	virtual void FireGameEvent ( SourceSdk::IGameEvent* ev ) override final;
 
 	void Push ( const char * msg );
 	void Flush ();

commit 0ccc111ad301966429cf7c020d1454654bdde6b2

Fix null pointer deref (regression from c6fd28c)

server-plugin/Code/plugin.cpp
@@ -433,7 +433,7 @@ void CNoCheatZPlugin::ClientActive ( SourceSdk::edict_t *pEntity )
 //---------------------------------------------------------------------------------
 void CNoCheatZPlugin::ClientDisconnect ( SourceSdk::edict_t *pEntity )
 {
-	DebugMessage ( Helpers::format("CNoCheatZPlugin::ClientDisconnect (pEntity:%p -> pEntity->classname:%s -> clientname:%s)", pEntity, pEntity->GetClassName (), PlayerHandler::const_iterator ( NczPlayerManager::GetInstance ()->GetPlayerHandlerByEdict ( pEntity ) )->GetName() ));
+	DebugMessage ( Helpers::format("CNoCheatZPlugin::ClientDisconnect (pEntity:%p -> pEntity->classname:%s)", pEntity, pEntity->GetClassName () ));
 
 	WallhackBlocker::GetInstance ()->ClientDisconnect ( Helpers::IndexOfEdict ( pEntity ) );
 	NczPlayerManager::GetInstance ()->ClientDisconnect ( pEntity );