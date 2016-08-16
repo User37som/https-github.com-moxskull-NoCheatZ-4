b1d7b2ffa7ba340d229877f242b307fed4c0147b
L-EARN <le.padellec.sylvain@gmail.com>
2016-08-16 05:20:21 +0200

    Don't block CUserCmds on detection

--- server-plugin/Code/Hooks/PlayerRunCommandHookListener.cpp
+++ server-plugin/Code/Hooks/PlayerRunCommandHookListener.cpp
@@ -98,16 +98,16 @@ void HOOKFN_INT PlayerRunCommandHookListener::RT_nPlayerRunCommand ( void* This,
 			{
 				ret = it->m_value.listener->RT_PlayerRunCommandCallback ( ph, pCmd, &old_cmd );
 
-				if( ret > PlayerRunCommandRet::CONTINUE ) break;
+				//if( ret > PlayerRunCommandRet::CONTINUE ) break;
 			}
 			it = it->m_next;
 		}
 
-		if( ret == PlayerRunCommandRet::CONTINUE ) // don't copy something we block or change ...
-		{
+		//if( ret == PlayerRunCommandRet::CONTINUE ) // don't copy something we block or change ...
+		//{
 			// memcpy but skip the virtual table pointer : https://github.com/L-EARN/NoCheatZ-4/issues/16#issuecomment-226697469
 			memcpy ( ( char * ) ( &old_cmd ) + sizeof ( void * ), ( char * ) pCmd + sizeof ( void * ), sizeof ( SourceSdk::CUserCmd_csgo ) - sizeof ( void * ) );
-		}
+		//}
 	}
 
 	if( ret < PlayerRunCommandRet::BLOCK )
