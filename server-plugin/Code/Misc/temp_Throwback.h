6d7108bd04e3f137f138c49a6347508ff436527b
L-EARN <le.padellec.sylvain@gmail.com>
2016-10-10 13:25:05 +0200

    Update Throwback

--- server-plugin/Code/Misc/temp_Throwback.h
+++ server-plugin/Code/Misc/temp_Throwback.h
@@ -37,6 +37,7 @@ class Throwback_Arithmetic
 
 	static_assert ( count >= 2 , "count is history to keep + current value itself");
 
+public:
 	struct inner_type
 	{
 		value_type v;
@@ -103,14 +104,10 @@ private:
 	}
 
 public:
-	Throwback_Arithmetic () :	m_current ( m_stack )
+	void Reset ()
 	{
-		static_assert ( std::is_arithmetic< value_type>::value, "value_type is not arithmetic" );
-		static_assert ( std::is_arithmetic< time_type>::value, "time_type is not arithmetic" );
-		static_assert ( std::is_signed< time_type >::value, "time_type can't be negative" );
-
 		inner_type const * const cp_current ( m_current );
-		
+
 		do
 		{
 			m_current->v = std::numeric_limits<value_type>::max ();
@@ -118,6 +115,15 @@ public:
 			GoForward ();
 		}
 		while( m_current != cp_current );
+	}
+
+	Throwback_Arithmetic () :	m_current ( m_stack )
+	{
+		static_assert ( std::is_arithmetic< value_type>::value, "value_type is not arithmetic" );
+		static_assert ( std::is_arithmetic< time_type>::value, "time_type is not arithmetic" );
+		static_assert ( std::is_signed< time_type >::value, "time_type can't be negative" );
+
+		Reset ();
 	}
 
 	Throwback_Arithmetic ( this_type const & other ) : m_current ( m_stack + ( other.m_current - other.m_stack ) )
@@ -158,7 +164,7 @@ public:
 		m_current->t = time;
 	}
 
-	void CopyHistory ( value_type* dest, size_t max_amount, size_t& amount, time_type time_range = std::numeric_limits<time_type>::max (), time_type now = std::numeric_limits<time_type>::max () )
+	void CopyHistory ( inner_type* dest, size_t& amount, size_t max_amount = count, time_type time_range = std::numeric_limits<time_type>::max (), time_type now = std::numeric_limits<time_type>::max () )
 	{
 		inner_type const * const cp_current ( m_current );
 		amount = 0;
@@ -169,37 +175,46 @@ public:
 			{
 				break;
 			}
-			if( time_range )
-			*dest = m_current->v;
+			if( now - m_current->t > time_range )
+			{
+				break;
+			}
+			memcpy ( dest, m_current, sizeof ( inner_type ) );
+			//*dest = m_current->v;
 			++dest;
 			++amount;
 			GoBackward ();
 		}
 		while( m_current != cp_current );
-
-		return ( float ) sum / ( float ) count;
 	}
 
-	float Average () const
+	float Average ( value_type range_value_min = std::numeric_limits<value_type>::min(),
+					value_type range_value_max = std::numeric_limits<value_type>::max ())
 	{
 		inner_type const * const cp_current ( m_current );
-		value_type sum ( m_current->v );
+		value_type sum ( 0 );
 
-		GoBackward ();
-		while( m_current != cp_current )
+		size_t amount ( 0 );
+
+		do
 		{
 			if( m_current->v == std::numeric_limits<value_type>::max () && m_current->t == std::numeric_limits<time_type>::max () )
 			{
 				break;
 			}
-			sum += m_current->v;
+			if( m_current->v >= range_value_min && m_current->v <= range_value_max )
+			{
+				++amount;
+				sum += m_current->v;
+			}
 			GoBackward ();
 		}
+		while( m_current != cp_current );
 
-		return (float)sum / (float)count;
+		return (float)sum / (float) amount;
 	}
 
-	value_type Min () const
+	value_type Min ()
 	{
 		inner_type const * const cp_current ( m_current );
 		value_type min ( m_current->v );
@@ -221,7 +236,7 @@ public:
 		return min;
 	}
 
-	value_type Max () const
+	value_type Max ()
 	{
 		inner_type const * const cp_current ( m_current );
 		value_type max ( m_current->v );
@@ -241,6 +256,26 @@ public:
 		}
 
 		return max;
+	}
+
+	time_type TimeSpan ()
+	{
+		inner_type const * const cp_current ( m_current );
+		inner_type const * min ( m_current );
+		time_type const max ( m_current->t );
+
+		GoBackward ();
+		while( m_current != cp_current )
+		{
+			if( m_current->v == std::numeric_limits<value_type>::max () && m_current->t == std::numeric_limits<time_type>::max () )
+			{
+				break;
+			}
+			min = m_current;
+			GoBackward ();
+		}
+
+		return max - min->t;
 	}
 };
 
