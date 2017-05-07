ea631b86f4d8535b1ac28bb7f8c717ec85e68757
L-EARN <le.padellec.sylvain@gmail.com>
2017-05-08 01:43:51 +0200

    Update local disk config

--- scripts/prepare-test.bat
+++ scripts/prepare-test.bat
@@ -1,2 +1,2 @@
-B:\steamcmd\steamcmd.exe +login anonymous +force_install_dir .\%1\ +app_update %3 +quit
-xcopy "..\Builds\%2" "B:\steamcmd\%1\%4" /E /F /Y 
+E:\steamcmd\steamcmd.exe +login anonymous +force_install_dir .\%1\ +app_update %3 +quit
+xcopy "..\Builds\%2" "E:\steamcmd\%1\%4" /E /F /Y 
