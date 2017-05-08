75d45ccbf3c9c523f0b6e5982b169e7a69a307cd
L-EARN <le.padellec.sylvain@gmail.com>
2017-05-08 02:11:44 +0200

    Add version.h

---
+++ server-plugin/Code/version.h
@@ -0,0 +1,3 @@
+// Those strings are defined in version.cpp when prepare-build.bat is called with visual studio or in the makefile.
+extern char const * const NCZ_VERSION_GIT;
+extern char const * const NCZ_VERSION_GIT_SHORT;
