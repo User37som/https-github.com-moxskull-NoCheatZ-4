9c56bcfd85e8f0d6ac672f693fe3d2cab50815df
L-EARN <le.padellec.sylvain@gmail.com>
2017-05-12 19:28:03 +0200

    impl hook for SourceHook::AddHook and RemoveHook

--- server-plugin/Code/version.cpp
+++ server-plugin/Code/version.cpp
@@ -1,2 +1,3 @@
-char const * const NCZ_VERSION_GIT = "v4-beta-r3-patch2-1-gdb37344-dirty-debug";
+#include "version.h"
+char const * const NCZ_VERSION_GIT = "v4-beta-r3-patch2-17-g0e45424-dirty";
 char const * const NCZ_VERSION_GIT_SHORT = "v4-beta-r3-patch2";
