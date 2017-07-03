106a2d56f0350e32ca9ba8a092b1f6aee8ac1309
L-EARN <le.padellec.sylvain@gmail.com>
2017-07-03 07:00:32 +0200

    Fix not initialized var in Logger

--- server-plugin/Code/Systems/Logger.h
+++ server-plugin/Code/Systems/Logger.h
@@ -75,7 +75,7 @@ private:
 	size_t m_current_memory_used;
 
 public:
-	Logger () : BaseStaticSystem ( "Logger", "Verbose - AlwaysFlush - AllowChat" ), singleton_class (), m_msg (), prolog ( basic_string("[NoCheatZ ").append(NCZ_VERSION_GIT_SHORT).append("] ") ), m_always_flush(false), m_msg_func( nullptr ), m_current_memory_used ( 0 )
+	Logger () : BaseStaticSystem ( "Logger", "Verbose - AlwaysFlush - AllowChat" ), singleton_class (), m_msg (), prolog ( basic_string("[NoCheatZ ").append(NCZ_VERSION_GIT_SHORT).append("] ") ), m_always_flush(false), m_allow_chat(true), m_msg_func( nullptr ), m_current_memory_used ( 0 )
 	{
 		ConnectToServerConsole ();
 	};
