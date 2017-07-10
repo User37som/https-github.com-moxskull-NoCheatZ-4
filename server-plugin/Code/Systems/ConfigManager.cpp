fd2f67ad296eacb2fb99e35610bb0a47d6676957
L-EARN <le.padellec.sylvain@gmail.com>
2017-07-11 01:40:40 +0200

    Fix #153

--- server-plugin/Code/Systems/ConfigManager.cpp
+++ server-plugin/Code/Systems/ConfigManager.cpp
@@ -15,7 +15,7 @@
 #	define ATTRIB_POST "_linux"
 #endif
 
-bool GetIniAttributeValue ( std::ifstream & file, basic_string const & root, basic_string const & attrib, basic_string & value )
+bool GetIniAttributeValue ( std::ifstream & file, basic_string const & root, basic_string const & attrib, basic_string & value, bool first_chance = true )
 {
 	LoggerAssert ( file.is_open () );
 
@@ -58,6 +58,9 @@ bool GetIniAttributeValue ( std::ifstream & file, basic_string const & root, bas
 				file.getline ( buf, FILE_LINE_BUFFER );
 				buf_ref = buf;
 				buf_ref.lower ();
+				
+				if(buf_ref[0] == '[')
+					break;
 
 				if( buf_ref.find ( attrib_expect ) == 0 )
 				{
@@ -78,7 +81,7 @@ bool GetIniAttributeValue ( std::ifstream & file, basic_string const & root, bas
 		Logger::GetInstance ()->Msg<MSG_ERROR> (  "ConfigManager::GetIniAttributeValue : Cannot read ini file" );
 	}
 	value = "";
-	Logger::GetInstance ()->Msg<MSG_ERROR> ( Helpers::format("ConfigManager::GetIniAttributeValue : Cannot find attribute %s -> %s", root.c_str(), attrib.c_str() ));
+	if(!first_chance) Logger::GetInstance ()->Msg<MSG_ERROR> ( Helpers::format("ConfigManager::GetIniAttributeValue : Cannot find attribute %s -> %s", root.c_str(), attrib.c_str() ));
 	return false;
 }
 
@@ -120,44 +123,44 @@ bool ConfigManager::LoadConfig ()
 	if( file )
 	{
 		basic_string value;
-		if( !GetIniAttributeValue ( file, "CONFIG", "config_version", value ) ) return false;
+		if( !GetIniAttributeValue ( file, "CONFIG", "config_version", value, false ) ) return false;
 		if( atoi ( value.c_str () ) == content_version )
 		{
 			// load virtual functions id
 			if (!GetIniAttributeValue(file, "CONFIG", "getdatadescmap" ATTRIB_POST, value))
-				if( !GetIniAttributeValue ( file, gamename, "getdatadescmap" ATTRIB_POST, value ) ) return false;
+				if( !GetIniAttributeValue ( file, gamename, "getdatadescmap" ATTRIB_POST, value, false ) ) return false;
 			vfid_getdatadescmap = atoi ( value.c_str () );
 
 			if (!GetIniAttributeValue(file, "CONFIG", "settransmit" ATTRIB_POST, value))
-				if( !GetIniAttributeValue ( file, gamename, "settransmit" ATTRIB_POST, value ) ) return false;
+				if( !GetIniAttributeValue ( file, gamename, "settransmit" ATTRIB_POST, value, false ) ) return false;
 			vfid_settransmit = atoi ( value.c_str () );
 
 			if (!GetIniAttributeValue(file, "CONFIG", "mhgroundentity" ATTRIB_POST, value))
-				if( !GetIniAttributeValue ( file, gamename, "mhgroundentity" ATTRIB_POST, value ) ) return false;
+				if( !GetIniAttributeValue ( file, gamename, "mhgroundentity" ATTRIB_POST, value, false ) ) return false;
 			vfid_mhgroundentity = atoi ( value.c_str () );
 
 			if (!GetIniAttributeValue(file, "CONFIG", "weaponequip" ATTRIB_POST, value))
-				if( !GetIniAttributeValue ( file, gamename, "weaponequip" ATTRIB_POST, value ) ) return false;
+				if( !GetIniAttributeValue ( file, gamename, "weaponequip" ATTRIB_POST, value, false ) ) return false;
 			vfid_weaponequip = atoi ( value.c_str () );
 
 			if (!GetIniAttributeValue(file, "CONFIG", "weapondrop" ATTRIB_POST, value))
-				if( !GetIniAttributeValue ( file, gamename, "weapondrop" ATTRIB_POST, value ) ) return false;
+				if( !GetIniAttributeValue ( file, gamename, "weapondrop" ATTRIB_POST, value, false ) ) return false;
 			vfid_weapondrop = atoi ( value.c_str () );
 
 			if (!GetIniAttributeValue(file, "CONFIG", "playerruncommand" ATTRIB_POST, value))
-				if( !GetIniAttributeValue ( file, gamename, "playerruncommand" ATTRIB_POST, value ) ) return false;
+				if( !GetIniAttributeValue ( file, gamename, "playerruncommand" ATTRIB_POST, value, false ) ) return false;
 			vfid_playerruncommand = atoi ( value.c_str () );
 
 			if (!GetIniAttributeValue(file, "CONFIG", "dispatch" ATTRIB_POST, value))
-				if( !GetIniAttributeValue ( file, gamename, "dispatch" ATTRIB_POST, value ) ) return false;
+				if( !GetIniAttributeValue ( file, gamename, "dispatch" ATTRIB_POST, value, false ) ) return false;
 			vfid_dispatch = atoi ( value.c_str () );
 
 			if (!GetIniAttributeValue(file, "CONFIG", "thinkpost" ATTRIB_POST, value))
-				if( !GetIniAttributeValue ( file, gamename, "thinkpost" ATTRIB_POST, value ) ) return false;
+				if( !GetIniAttributeValue ( file, gamename, "thinkpost" ATTRIB_POST, value, false ) ) return false;
 			vfid_thinkpost = atoi ( value.c_str () );
 
 			if (!GetIniAttributeValue(file, "CONFIG", "tickrate_override", value))
-				if (!GetIniAttributeValue(file, gamename, "tickrate_override", value)) return false;
+				if (!GetIniAttributeValue(file, gamename, "tickrate_override", value, false)) return false;
 			tickrate_override = atoi(value.c_str());
 			if (tickrate_override == 0) return false;
 			tickinterval = 1.0f / tickrate_override;
@@ -165,24 +168,24 @@ bool ConfigManager::LoadConfig ()
 			// load some strings
 
 			if (!GetIniAttributeValue(file, "CONFIG", "playerdataclass", m_playerdataclass))
-				if( !GetIniAttributeValue ( file, gamename, "playerdataclass", m_playerdataclass ) ) return false;
+				if( !GetIniAttributeValue ( file, gamename, "playerdataclass", m_playerdataclass, false ) ) return false;
 
 			// load some values
 
 			if (!GetIniAttributeValue(file, "CONFIG", "f_smoketime", value))
-				if( !GetIniAttributeValue ( file, gamename, "f_smoketime", value ) ) return false;
+				if( !GetIniAttributeValue ( file, gamename, "f_smoketime", value, false ) ) return false;
 			m_smoke_time = ( float ) atof ( value.c_str () );
 
 			if (!GetIniAttributeValue(file, "CONFIG", "f_smoke_time_to_bang", value))
-				if( !GetIniAttributeValue ( file, gamename, "f_smoke_time_to_bang", value ) ) return false;
+				if( !GetIniAttributeValue ( file, gamename, "f_smoke_time_to_bang", value, false ) ) return false;
 			m_smoke_timetobang = ( float ) atof ( value.c_str () );
 
 			if (!GetIniAttributeValue(file, "CONFIG", "f_inner_smoke_radius_sqr", value))
-				if( !GetIniAttributeValue ( file, gamename, "f_inner_smoke_radius_sqr", value ) ) return false;
+				if( !GetIniAttributeValue ( file, gamename, "f_inner_smoke_radius_sqr", value, false ) ) return false;
 			m_innersmoke_radius_sqr = ( float ) atof ( value.c_str () );
 
 			if (!GetIniAttributeValue(file, "CONFIG", "f_smoke_radius", value))
-				if( !GetIniAttributeValue ( file, gamename, "f_smoke_radius", value ) ) return false;
+				if( !GetIniAttributeValue ( file, gamename, "f_smoke_radius", value, false ) ) return false;
 			m_smoke_radius = ( float ) atof ( value.c_str () );
 
 			// disable systems
@@ -233,7 +236,7 @@ bool ConfigManager::LoadConfig ()
 				}
 			}
 
-			if (GetIniAttributeValue(file, gamename, "admin_ids", value))
+			if (GetIniAttributeValue(file, "CONFIG", "admin_ids", value))
 			{
 				value.lower();
 				SplitString<char>(value, ';', m_admins);
