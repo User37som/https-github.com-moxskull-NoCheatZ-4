819de55c30ed30083f9815fe5c4d523ac3de471b
L-EARN <le.padellec.sylvain@gmail.com>
2017-08-06 16:16:35 +0200

    prepare-test don't quit on error

--- scripts/prepare-test.bat
+++ scripts/prepare-test.bat
@@ -1,4 +1,4 @@
 if not defined CI (
 	E:\steamcmd\steamcmd.exe +login anonymous +force_install_dir .\%1\ +app_update %3 +quit
-	xcopy "..\Builds\%2" "E:\steamcmd\%1\%4" /E /F /Y
+	xcopy "..\Builds\%2" "E:\steamcmd\%1\%4" /E /C /F /Y
 )
